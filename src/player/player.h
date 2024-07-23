d3aa5a48d0c4a28a65252d0104c0dd7ee59367a9
Aditya <adityakeerthi05@gmail.com>
2024-07-23 18:40:15 -0400

    chore: adding comments

--- src/player/player.h
+++ src/player/player.h
@@ -8,12 +8,12 @@ class Player {
     bool isHuman; // boolean representing if player is human or computer
     char color; // color is either "W" or "B", representing which 
     // vector<Piece> captured;
-    int score;
+    int score; // how many points the player scored
   public:
-    virtual ~Player();
-    virtual std::pair<Position, Position> getMove() = 0;
-    bool isHuman();
-    char getColor();
+    virtual ~Player(); // virtual destructor to make it an abstract class
+    virtual std::pair<Position, Position> getMove() = 0; // virtual getMove
+    bool isHuman(); // getter for if they're human
+    char getColor(); // getter for they're color
 };
 
 #endif
