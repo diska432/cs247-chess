9a55e07ffa5550bfaaeefb1c551bffb271948806
Aditya <adityakeerthi05@gmail.com>
2024-07-28 17:24:34 -0400

    fix: isWhite

--- src/render/render.cc
+++ src/render/render.cc
@@ -9,7 +9,7 @@ Render::~Render() {};
 bool Render::isWhite(const Position &p) const {
   // black squares are when the x%2=y%2
   if (p.getX() % 2 == p.getY() % 2) {
-    return false; 
+    return true; 
   }
-  return true;
+  return false;
 }
