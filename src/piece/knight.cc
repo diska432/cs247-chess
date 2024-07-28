c9a674b5c97588e8f4cc9f7b3fa0748db869f98e
diska432 <iskanderdauletov@gmail.com>
2024-07-28 14:39:12 -0400

    add knight valid moves

--- src/piece/knight.cc
+++ src/piece/knight.cc
@@ -8,7 +8,46 @@ Knight::Knight(char team) : Piece('n', team) {};
 
 vector<Position> Knight::getAllMoves(Chessboard &cb, Position p) {
   vector<Position> res;
-  Position temp{0, 0};
-  res.push_back(temp);
+if(p.getX() + 2 < 8 ) {
+//check if square is empty or there is an opposing piece
+if(p.getY() + 1 < 8 && (!cb.getSquare({p.getX() + 2, p.getY() + 1}) || (cb.getSquare({p.getX() + 2, p.getY() + 1}) && 
+cb.getSquare({p.getX() + 2, p.getY() + 1})->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+    res.push_back({p.getX() + 2, p.getY() + 1});
+}
+//check if square is empty or there is an opposing piece
+if(p.getY() - 1 >= 0 && (!cb.getSquare({p.getX() + 2, p.getY() - 1}) || (cb.getSquare({p.getX() + 2, p.getY() - 1}) && cb.getSquare({p.getX() + 2, p.getY() - 1})->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+    res.push_back({p.getX() + 2, p.getY() - 1});
+}
+} 
+if(p.getX() - 2 >= 0) {
+    //check if square is empty or there is an opposing piece
+    if(p.getY() + 1 < 8 && (!cb.getSquare({p.getX() - 2, p.getY() + 1}) || (cb.getSquare({p.getX() - 2, p.getY() + 1}) && cb.getSquare({p.getX() - 2, p.getY() + 1}) ->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+        res.push_back({p.getX() - 2, p.getY() + 1});
+    }
+    //check if square is empty or there is an opposing piece
+    if(p.getY() - 1 >= 0 && (!cb.getSquare({p.getX() - 2, p.getY() - 1}) || (cb.getSquare({p.getX() - 2, p.getY() - 1}) && cb.getSquare({p.getX() - 2, p.getY() - 1}) ->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+        res.push_back({p.getX() - 2, p.getY() - 1});
+    }
+}
+if(p.getY() + 2 < 8) {
+    //check if square is empty or there is an opposing piece
+    if(p.getX() + 1 < 8 && (!cb.getSquare({p.getX() + 1, p.getY() + 2}) || (cb.getSquare({p.getX() + 1, p.getY() + 2}) && cb.getSquare({p.getX() + 1, p.getY() + 2}) ->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+        res.push_back({p.getX() + 1, p.getY() + 2});
+    }
+    //check if square is empty or there is an opposing piece
+    if(p.getX() - 1 >= 0 && (!cb.getSquare({p.getX() - 1, p.getY() + 2}) || (cb.getSquare({p.getX() - 1, p.getY() + 2}) && cb.getSquare({p.getX() - 1, p.getY() + 2}) ->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+        res.push_back({p.getX() - 1, p.getY() + 2});
+    }
+}
+if(p.getY() - 2 >= 0) {
+    //check if square is empty or there is an opposing piece
+    if(p.getX() + 1 < 8 && (!cb.getSquare({p.getX() + 1, p.getY() - 2}) || (cb.getSquare({p.getX() + 1, p.getY() - 2}) && cb.getSquare({p.getX() + 1, p.getY() - 2}) ->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+        res.push_back({p.getX() + 1, p.getY() - 2});
+    }
+    //check if square is empty or there is an opposing piece
+    if(p.getX() - 1 >= 0 && (!cb.getSquare({p.getX() - 1, p.getY() - 2}) || (cb.getSquare({p.getX() - 1, p.getY() - 2}) && cb.getSquare({p.getX() - 1, p.getY() - 2}) ->getTeam() != cb.getSquare({p.getX(), p.getY()})->getTeam()))) {
+        res.push_back({p.getX() - 1, p.getY() - 2});
+    }
+}
   return res;
 }
