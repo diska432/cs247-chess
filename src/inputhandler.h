046b76f62859bc415fd86d555ae22a57e803cd7d
Aditya <adityakeerthi05@gmail.com>
2024-07-28 21:20:28 -0400

    fix: createLevel

--- src/inputhandler.h
+++ src/inputhandler.h
@@ -16,6 +16,7 @@ class InputHandler {
     void enterSetup();
   public: 
     InputHandler(std::shared_ptr<Chess> game, std::shared_ptr<TextRender> textrender, std::shared_ptr<GraphicRender> graphicrender);
+    std::shared_ptr<Computer> createLevel(int level);
     int handleInput();
 };
 
